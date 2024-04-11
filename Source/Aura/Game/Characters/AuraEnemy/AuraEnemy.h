b0b1658074407bcccd5f49b742da8da04e18fe08
Tatevik Avoyan <tatevik@neographgames.com>
2024-04-11 15:18:26 +0400

    Implemented Inteface

--- Source/Aura/Game/Characters/AuraEnemy/AuraEnemy.h
+++ Source/Aura/Game/Characters/AuraEnemy/AuraEnemy.h
@@ -4,14 +4,18 @@
 
 #include "CoreMinimal.h"
 #include "Aura/Game/Characters/CharacterBase/AuraCharacterBase.h"
+#include "Aura/Game/Interaction/EnemyInterface.h"
 #include "AuraEnemy.generated.h"
 
 /**
  * 
  */
 UCLASS()
-class AURA_API AAuraEnemy : public AAuraCharacterBase
+class AURA_API AAuraEnemy : public AAuraCharacterBase, public IEnemyInterface
 {
 	GENERATED_BODY()
-	
+
+	virtual void HighlightActor() override;
+
+	virtual void UnHighlightActor() override;
 };
