aad7b0216bf494afd61e4e785569ad96d984553c
Tatevik Avoyan <tatevik@neographgames.com>
2024-05-21 15:06:54 +0400

    Section 6:
    45. PreAttributeChange

--- Source/Aura/Game/AbilitySystem/AttributeSet/AuraAttributeSet.cpp
+++ Source/Aura/Game/AbilitySystem/AttributeSet/AuraAttributeSet.cpp
@@ -3,6 +3,7 @@
 
 #include "AuraAttributeSet.h"
 #include "Net/UnrealNetwork.h"
+#include "Math/UnrealMathUtility.h"
 
 UAuraAttributeSet::UAuraAttributeSet()
 {
@@ -20,6 +21,20 @@ void UAuraAttributeSet::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& Ou
 	DOREPLIFETIME_CONDITION_NOTIFY(UAuraAttributeSet, MaxHealth, COND_None, REPNOTIFY_Always);
 	DOREPLIFETIME_CONDITION_NOTIFY(UAuraAttributeSet, Mana, COND_None, REPNOTIFY_Always);
 	DOREPLIFETIME_CONDITION_NOTIFY(UAuraAttributeSet, MaxMana, COND_None, REPNOTIFY_Always);
+}
+
+void UAuraAttributeSet::PreAttributeChange(const FGameplayAttribute& Attribute, float& NewValue)
+{
+	Super::PreAttributeChange(Attribute, NewValue);
+	
+	if (Attribute == GetHealthAttribute())
+	{
+		NewValue = FMath::Clamp(NewValue, 0.f, GetMaxHealth());
+	}
+	if (Attribute == GetManaAttribute())
+	{
+		NewValue = FMath::Clamp(NewValue, 0.f, GetMaxMana());
+	}
 }
 
 void UAuraAttributeSet::OnRep_Health(const FGameplayAttributeData& OldHealth) const
