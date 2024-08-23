c415225d9cdf6e3d690c2f97733c23462ecc06a0
Tatevik Avoyan <tatevik@neographgames.com>
2024-08-23 15:44:05 +0400

    Section 9:
    90. Attribute Menu Widget Controller
    91. Aura Ability System Blueprint Library

---
+++ Source/Aura/Game/AbilitySystem/AuraAbilitySystemLibrary.cpp
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Game/AbilitySystem/AuraAbilitySystemLibrary.h"
+
+#include "Game/Characters/PlayerState/AuraPlayerState.h"
+#include "Game/UI/HUD/AuraHUD.h"
+#include "Game/UI/WidgetController/AuraWidgetController/AuraWidgetController.h"
+#include "Kismet/GameplayStatics.h"
+
+UOverlayWidgetController* UAuraAbilitySystemLibrary::GetOverlayWidgetController(const UObject* WorldContextObject)
+{
+	if (APlayerController* PC = UGameplayStatics::GetPlayerController(WorldContextObject, 0); IsValid(PC))
+	{
+		if (AAuraHUD* AuraHUD = Cast<AAuraHUD>(PC->GetHUD()))
+		{
+			if (AAuraPlayerState* PS = PC->GetPlayerState<AAuraPlayerState>(); IsValid(PS))
+			{
+				UAbilitySystemComponent* ASC = PS->GetAbilitySystemComponent();
+				UAttributeSet* AS = PS->GetAttributeSet();
+				const FWidgetControllerParams WidgetControllerParams(PC, PS, ASC, AS);
+				return AuraHUD->GetOverlayWidgetController(WidgetControllerParams);
+			}
+		}
+	}
+
+	return nullptr;
+}
---
+++ Source/Aura/Game/UI/WidgetController/AttributeMenuWidgetController/AttributeMenuWidgetController.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Game/UI/WidgetController/AttributeMenuWidgetController/AttributeMenuWidgetController.h"
+
+void UAttributeMenuWidgetController::BindCallbacksToDependencies()
+{
+}
+
+void UAttributeMenuWidgetController::BroadcastInitialValues()
+{
+}
